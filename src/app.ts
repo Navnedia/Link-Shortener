a3d2ac47b25f3dce1ad20363fef4002fb708dd26
Navnedia <87249556+Navnedia@users.noreply.github.com>
2022-07-28 02:06:12 -0500

    Set application to listen on port

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import os from 'os';
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config(); // Load environment variables.
+
+const PORT = process.env.PORT || 8080;
+const app = express(); // Initialize express app.
+
+app.use(express.json()); // Parse body as JSON.
+
+app.listen(PORT, () => {
+    console.log(`Link Shortener App Listening on Port: ${PORT}`);
+})
