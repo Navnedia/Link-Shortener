127be4f9748c62324682ec593c38ba894b68ae76
Navnedia <87249556+Navnedia@users.noreply.github.com>
2022-07-31 18:15:39 -0500

    Added return to error handler

--- src/middleware/error-handler.ts
+++ src/middleware/error-handler.ts
@@ -1,8 +1,9 @@
+//! Is this needed?
 export const errorHandler = (error, req, res, next) => {
     const statusCode = res.statusCode ? res.statusCode : 500;
     // console.log(error.stack!);
     // console.log("\n" + JSON.stringify(error, null, "\t"));
     
     // Should I return?
-    res.status(statusCode).send(JSON.stringify(error));
+    return res.status(statusCode).send(JSON.stringify(error));
 };
