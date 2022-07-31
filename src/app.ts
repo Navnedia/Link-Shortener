7a541137cf85349d5f0aa84406509ae6ae15db46
Navnedia <87249556+Navnedia@users.noreply.github.com>
2022-07-31 18:25:32 -0500

    Added temp 404 route for undefined endpoints

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
 import express from 'express';
-import {errorHandler} from './middleware/error-handler.js';
-import connectDB from './db.js';
 import dotenv from 'dotenv';
-dotenv.config(); // Load environment variables.
+import connectDB from './db.js';
+import AppError from './utils/appError.js';
+import {errorHandler} from './middleware/error-handler.js';
 
+dotenv.config(); // Load environment variables.
 connectDB(); // Initalize connection to database.
 
 const PORT = process.env.PORT || 8080;
@@ -13,6 +14,12 @@ app.use(express.json()); // Parse body as JSON.
 
 app.listen(PORT, () => {
     console.log(`Link Shortener App Listening on Port: ${PORT}`);
+});
+
+// Show error for undefined endpoints:
+app.use('*', (req, res) => {
+    return res.status(404).send(new AppError(404, 'Not Found', undefined, 
+        `The endpoint ${req.baseUrl + req.path} could not be found`));
 });
 
 app.use(errorHandler); // Initialize error handler middleware.
