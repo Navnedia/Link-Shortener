521683c4b442d5bb253f8263464eb414989b7fe5
Navnedia <87249556+Navnedia@users.noreply.github.com>
2022-08-01 20:00:01 -0500

    Added a method retrive a formated API response with extra information
    I decided to add this instance method because I didn't want to store duplicate data in the database for the actual shortlink url, so this allows me just calculate the link string and include it in the response. Improving API usablity without adding extra data to the database. I could've also used virtuals, but including virtuals in the toJSON output also seemed to add a lot of other internal properties that wouldn't be useful to the end user.

--- src/models/ShortLink.ts
+++ src/models/ShortLink.ts
@@ -11,6 +11,7 @@ interface IShortLink {
 // Define ShortLink instance methods as interface:
 interface IShortLinkMethods {
     showRedirect(): string;
+    getAPIResponse(): object;
 }
 
 // Create a new Model type that knows about the instance methods:
@@ -27,11 +28,40 @@ const shortLinkSchema = new mongoose.Schema<IShortLink, ShortLinkModel, IShortLi
     clicks: {type: Number, default: 0}
 });
 
+/**
+ * ! The virtuals aproach has been set aside for now because it returns a
+ * ! lot of unuseful data to the user like the internal object id, etc.
+ */
+// /**
+//  * Add a virtual property to the schema. This link property contains the 
+//  * full shortlink including the domain name. This property has been 
+//  * virtualized to avoid storing unnessasary duplicate data in the database.
+//  */
+// shortLinkSchema.virtual('link').get(function() {
+//     return `${process.env.DOMAIN_NAME}/${this.shortID}`;
+// });
+
+// // Make sure the virtual link property is included in the API response:
+// shortLinkSchema.set('toJSON', {virtuals: true});
+// shortLinkSchema.set('toObject', {virtuals: true });
+
 
 // Define method implementations:
 
 shortLinkSchema.method('showRedirect', function showRedirect() {
     return `shortID: ${this.shortID} --> destination: ${this.destination}`;
+});
+
+shortLinkSchema.method('getAPIResponse', function getAPIResponse() {
+    const link = `${process.env.DOMAIN_NAME}/${this.shortID}`;
+
+    return {
+        name: this.name,
+        shortID: this.shortID,
+        destination: this.destination,
+        clicks: this.clicks,
+        link: link
+    };
 });
 
 shortLinkSchema.static('findByShortID', async function findByShortID(shortID: string) {
