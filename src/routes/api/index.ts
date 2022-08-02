ead303a5fa2eae5b6ee9f1b790673f7d35d6d84b
Navnedia <87249556+Navnedia@users.noreply.github.com>
2022-08-01 19:05:19 -0500

    Added a middleware package trim request body properties

--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+import trimmer from 'request_trimmer';
 import shortlinkRoutes from './shortlink-routes.js';
 
 const router = express.Router();
+
+router.use(trimmer.trim_body); // Middleware to trim request body properties.
 
 router.use('/shortlinks', shortlinkRoutes);
 
