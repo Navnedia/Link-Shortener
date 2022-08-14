c6a73af668bd166435a04309b0bfc0f8c77eb5ca
Navnedia <87249556+Navnedia@users.noreply.github.com>
2022-08-14 18:00:44 -0500

    Started adding some code to create a new link

---
+++ frontend/scripts/client-validators.js
@@ -0,0 +1,14 @@
+/**
+ * Validates input data to see if it's a valid url string
+ * @param data - data to validate for URL string
+ * @returns boolean (true) if input valid url string
+ */
+export function validURL(data) {
+    const pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+      '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+      '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+      '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+    return !!pattern.test(data);
+}
--- frontend/scripts/render.js
+++ frontend/scripts/render.js
@@ -1,6 +1,8 @@
 import {createLink, getAllLinks} from "./api.js";
+import {validURL} from "./client-validators.js";
 
 const linkContainer = document.getElementById('linksContainer');
+const createBtn = document.getElementById('btnCreate');
 
 
 document.addEventListener('DOMContentLoaded', async () => {
@@ -8,12 +10,51 @@ document.addEventListener('DOMContentLoaded', async () => {
 
     //! I should show something diffrent if the links are empty or if a backend error occurs.
     //! should I be doing any kind of validation here?
+
     if (Array.isArray(links)) {
         for (const linkData of links) {
             const linkItem = document.createElement('link-item');
             linkItem.setData(linkData);
             
-            linkContainer.appendChild(linkItem);
+            linkContainer.prepend(linkItem);
         }
     }
 });
+
+// Real time url validation:
+document.getElementById('txtLongUrl').addEventListener('input', (e) => {
+    const element = e.target;
+    (!validURL(element.value)) ? element.classList.add('invalid') 
+        : element.classList.remove('invalid');
+});
+
+
+createBtn.addEventListener('click', addLink);
+
+async function addLink() {
+    const data = {};
+    data['destination'] = document.getElementById('txtLongUrl').value.trim() || null; 
+    data['name'] = document.getElementById('txtLinkName').value.trim() || null;
+
+    //! Add form validation.
+    //! Handle returned errors.
+
+    if (!data.destination) {
+        return;
+    }
+    // If the destination url doesn't include a protocol, then add the http:// protocol by default:
+    if (!(/^(https?:\/\/)/.test(data.destination))) {
+        data.destination = 'http://' + data.destination;
+    }
+    if (!validURL(data.destination)) {
+        return;
+    }
+
+    const linkRes = await createLink(data);
+
+    if (linkRes) {
+        const linkItem = document.createElement('link-item');
+        linkItem.setData(linkRes);
+        linkContainer.prepend(linkItem);
+    }
+}
