60f43676ff18cbbc205fb282b6c830013c455afe
Navnedia <87249556+Navnedia@users.noreply.github.com>
2022-08-15 21:44:01 -0500

    Added comments and a few other UI visuals.

--- frontend/scripts/render.js
+++ frontend/scripts/render.js
@@ -2,39 +2,41 @@ import {createLink, getAllLinks} from "./api.js";
 import {validURL} from "./client-validators.js";
 
 const linkContainer = document.getElementById('linksContainer');
-const createBtn = document.getElementById('btnCreate');
+const longURL = document.getElementById('txtLongUrl');
+const linkName = document.getElementById('txtLinkName');
+const shortenBtn = document.getElementById('btnShorten');
 
 
 document.addEventListener('DOMContentLoaded', async () => {
-    var links = await getAllLinks();
+    var links = await getAllLinks(); // Request all the links.
 
     //! I should show something diffrent if the links are empty or if a backend error occurs.
     //! should I be doing any kind of validation here?
 
-    if (Array.isArray(links)) {
+    if (Array.isArray(links)) { 
+        // Display each link in the response array:
         for (const linkData of links) {
             const linkItem = document.createElement('link-item');
             linkItem.setData(linkData);
-            
             linkContainer.prepend(linkItem);
         }
     }
 });
 
-// Real time url validation:
-document.getElementById('txtLongUrl').addEventListener('input', (e) => {
-    const element = e.target;
-    (!validURL(element.value)) ? element.classList.add('invalid') 
-        : element.classList.remove('invalid');
-});
+// // Real time url validation:
+// document.getElementById('txtLongUrl').addEventListener('input', (e) => {
+//     const element = e.target;
+//     (!validURL(element.value)) ? element.classList.add('invalid') 
+//         : element.classList.remove('invalid');
+// });
 
 
-createBtn.addEventListener('click', addLink);
+shortenBtn.addEventListener('click', addLink);
 
 async function addLink() {
     const data = {};
-    data['destination'] = document.getElementById('txtLongUrl').value.trim() || null; 
-    data['name'] = document.getElementById('txtLinkName').value.trim() || null;
+    data['destination'] = longURL.value.trim() || null; 
+    data['name'] = linkName.value.trim() || null;
 
     //! Add form validation.
     //! Handle returned errors.
@@ -50,11 +52,21 @@ async function addLink() {
         return;
     }
 
-    const linkRes = await createLink(data);
+    shortenBtn.classList.add('loading'); // Add button loader style.
 
-    if (linkRes) {
+    const linkRes = await createLink(data); // Send create request.
+    if (Object.keys(linkRes).length !== 0) { // If non-empty data was recived, create and display the element:
         const linkItem = document.createElement('link-item');
         linkItem.setData(linkRes);
         linkContainer.prepend(linkItem);
+
+        // Scroll to the top to show the new link:
+        window.scroll({top: 0, behavior: 'smooth'});
+
+        // Clear input fields only if a link was created.
+        longURL.value = '';
+        linkName.value = '';
     }
+
+    shortenBtn.classList.remove('loading'); // Remove button loader style.
 }
