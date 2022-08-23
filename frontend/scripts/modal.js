aba8356ac957dda42755158f8e1166d3423d4ed7
Navnedia <87249556+Navnedia@users.noreply.github.com>
2022-08-23 01:10:49 -0500

    Added modal script to handle opening and closing

---
+++ frontend/scripts/modal.js
@@ -0,0 +1,29 @@
+const modalWrapper = document.getElementById('modalWrapper');
+const modalBody = document.getElementById('modalBody');
+
+/**
+ * Constructs and opens the specified modal with the link
+ * data attached.
+ * @param {string} modalType - The string value of the modal element you wish to open.
+ * @param {object} linkItem - The referece to the linkItem we are editing.
+ */
+export async function openModal(modalType, linkItem) {
+    const modalElement = document.createElement(modalType);
+    modalElement.setData(linkItem);
+
+    closeModal(); // Close any existing modals.
+    modalBody.append(modalElement);
+    modalWrapper.classList.remove('hidden');
+}
+
+/**
+ * Closes all open modals.
+ */
+export async function closeModal() {
+    modalWrapper.classList.add('hidden');    
+    modalBody.innerHTML = "";
+}
+
+// Async??
+// Where to place these methods??
+// Make 
